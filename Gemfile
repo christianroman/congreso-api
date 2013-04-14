e7261ad7ea270326be031502e42e00630cde4b62
Christian Roman <chroman16@gmail.com>
2013-04-14 12:53:49 -0500

    first commit

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "http://rubygems.org"
 
 gem "sinatra"
-gem "rabl"
 gem "builder"
 gem "nokogiri"
+gem "httparty"
--- app.rb
+++ app.rb
@@ -1,273 +1,148 @@
-$:.unshift File.join(File.dirname(__FILE__),'lib')
-
-require 'rubygems' 
+require 'httparty'
+require 'rubygems'
+require 'json' 
 require 'sinatra'
-require 'rabl'
-require 'active_support/core_ext'
-require 'active_support/inflector'
 require 'builder'
 require 'nokogiri'
 require 'open-uri'
 
-require 'diputado'
-require 'comision'
-require 'iniciativa'
-require 'proposicion'
-require 'votacion'
-require 'asistencia'
-require 'comision_integrante'
+ENDPOINT = 'http://aplicaciones4.sct.gob.mx/sibuac_internet/ControllerUI'
 
-Rabl.register!
-Rabl.configure do |config|
-    config.include_json_root = false
-    config.include_child_root = false
-end
+post '/rutas' do
 
-before do
-    content_type :json
-end
+	COMBUSTIBLE = 11.25
+	RENDIMIENTO = 14.00
 
-get '/api/diputados' do
-    part = nil
-    @diputados = Array.new
-    doc = Nokogiri::HTML(open('http://sitl.diputados.gob.mx/LXII_leg/listado_diputados_gpnp.php?tipot=TOTAL'))
-    doc.css("table table")[1].css("tr").each do |tr|
+	json = File.open('data.json', 'r')
+	data = JSON.parse json.read, :symbolize_names => true
 
-    	if tr.css("img").first
-	    	src = tr.css("img").first.attr("src")
-	    	case src
-	    	when 'images/pri01.png'
-				part = 'PRI'
-	    	when 'images/pan.png'
-				part = 'PAN'
-	    	when 'images/prd01.png'
-				part = 'PRD'
-	    	when 'images/logvrd.jpg'
-				part = "PVEM"
-	    	when 'images/logo_movimiento_ciudadano.png'
-				part = "MC"
-	    	when 'images/logpt.jpg'
-				part = 'PT'
-	    	when 'images/panal.gif'
-				part = 'NA'
-	    	end
-		end
+	#params = JSON.parse(request.env["rack.input"].read)
+	params = JSON.parse request.body.read
 
-		if tr.css("a").first
-		    @diputado = Diputado.new
-		    @diputado.id = tr.at_css("a").attr("href").gsub("curricula.php?dipt=", '')
-		    @diputado.nombre = tr.at_css("a").text.gsub(/^\d+\s/, '').squeeze(' ')
-		    @diputado.entidad = tr.css("td")[1].text.squeeze(' ')
-		    @diputado.distrito = tr.css("td")[2].text.squeeze(' ')
-		    @diputado.partido = part
-		    @diputados << @diputado
-		end
-    end 
+	ciudadOrigen = params["ciudad_origen"]
+	ciudadDestino = params["ciudad_destino"]
 
-    rabl :diputados
-end
+	ciudadOrigen = ciudadOrigen.ljust(4, '0')
+	ciudadDestino = ciudadDestino.ljust(4, '0')
 
+	estadoOrigen = data[:puntos][:"#{ciudadOrigen}"][:estado]
+  	estadoDestino = data[:puntos][:"#{ciudadDestino}"][:estado]
 
-get '/api/diputado/:id' do
-    doc = Nokogiri::HTML(open("http://sitl.diputados.gob.mx/LXII_leg/curricula.php?dipt=#{params[:id]}"))
-    node = doc.css("table tr")[2].css("table")[1]
-    
-    src = node.css("tr").first.css("td")[1].css("img").first.attr("src")
-    case src
-    when 'images/pri01.png'
-		pt = 'PRI'
-    when 'images/pan.png'
-    	pt = 'PAN'
-    when 'images/prd01.png'
-    	pt = 'PRD'
-    when 'images/logvrd.jpg'
-    	pt = "PVEM"
-    when 'images/logo_movimiento_ciudadano.png'
-    	pt = "MC"
-    when 'images/logpt.jpg'
-    	pt = 'PT'
-    when 'images/panal.gif'
-    	pt = 'NA'
-    end
+  	vehiculos = params["vehiculos"] ? params["vehiculos"] : 2
+  	calculaRendimiento = params["calcula_rendimiento"] ? params["calcula_rendimiento"] == 'true' ? 'si' : nil : nil
+  	tamanioVehiculo = params["tamanio_vehiculo"] ? params["tamanio_vehiculo"] : 2
+  	rendimiento = params["rendimiento"] ? params["rendimiento"] : RENDIMIENTO
+  	combustible = params["combustible"] ? params["combustible"] : COMBUSTIBLE
 
-    @diputado = Diputado.new
-    @diputado.id = params[:id]
-    @diputado.partido = pt
-    @diputado.foto = 'http://sitl.diputados.gob.mx/LXII_leg/' + node.css("tr").first.css("td").first.css("img").attr("src")
-    @diputado.nombre = node.css("tr").first.css("span").text.strip
-    @diputado.tipo_eleccion = node.css("tr")[1].text.split(':')[1].strip
-    @diputado.entidad = node.css("tr")[2].text.split(':')[1].strip
-    @diputado.distrito = node.css("tr")[3].text.split(':')[1].strip
-    @diputado.cabecera = node.css("tr")[4].text.split(':')[1].strip
-    @diputado.curul = node.css("tr")[5].text.split(':')[1].strip
-    @diputado.suplente = node.css("tr")[6].text.split(':')[1].strip
-    @diputado.onomastico = node.css("tr")[7].text.split(':')[1].strip
-    @diputado.email = node.css("tr")[8].text.split(':')[1].strip
-    
-    @comisiones = Array.new
+  	zonasUrbanas = params["zonas_urbanas"] ? params["zonas_urbanas"] == 'true' ? true : false : false
 
-    node.css("a").each do |a|
-	if a.attr("href").match(/^integrantes_de_comisionlxii\.php\?comt=\d+/)
-	    @comision = Comision.new
-	    @comision.id = a.attr("href").gsub("integrantes_de_comisionlxii.php?comt=", "")
-	    @comision.nombre = a.text.strip
-	    @comisiones << @comision
+	query = {
+		action: 'cmdSolRutas',
+		tipo: 1,
+		red: 'simplificada',
+		edoOrigen: estadoOrigen,
+		ciudadOrigen: ciudadOrigen,
+		edoDestino: estadoDestino,
+		ciudadDestino: ciudadDestino,
+		vehiculos: vehiculos,
+		calculaRendimiento: calculaRendimiento,
+		tamanioVehiculo: tamanioVehiculo,
+		rendimiento: rendimiento,
+		combustible: combustible
+	}
+
+	respuesta = HTTParty.get(ENDPOINT, :query => query).body
+
+	html = Nokogiri::HTML(respuesta)
+
+	tramos = []
+	puntos = []
+
+	html.css('input[name=destino]')[0].attr('value').split('&').each do |punto|
+
+		inicio, inicio_x, inicio_y, fin, fin_x, fin_y, inicio_tipo, fin_tipo, tipo_punto = punto.split('#')
+
+		puntos.push({
+			inicio: {
+				nombre: inicio,
+				lat: inicio_x,
+				lng: inicio_y,
+				tipo: inicio_tipo
+			},
+	    	fin: {
+	    		nombre: fin,
+	    		lat: fin_x,
+	    		lng: fin_y,
+	    		tipo: fin_tipo
+	    	},
+	    	tipo: tipo_punto
+  		})
+
 	end
-    end
 
-    rabl :diputado
-end
+	trs = html.css('#tContenido tr')
 
+	distanciaTotal = 0.0
+	tiempoTotal = 0
+	peajeTotal = 0.0
+	combustibleTotal = 0.0
+	costoTotal = 0.0
 
-get '/api/diputado/:id/iniciativas' do
+	trs[2..trs.count-5].each do |tr|
 
-    @iniciativas = Array.new
+		tramo = {}
+		next unless tr.content.strip != ''
 
-    for t in 1..6 do
-	doc = Nokogiri::HTML(open("http://sitl.diputados.gob.mx/LXII_leg/iniciativas_por_pernplxii.php?iddipt=#{params[:id]}&pert=#{t}"))
-	tds = doc.css("table")[1].css("td")
-	
-	i = 0
-	begin
-	    if tds[i].text.strip != 'INICIATIVA'
-		@iniciativa = Iniciativa.new
-		@iniciativa.nombre = tds[i].text.strip.squeeze(' ')
-		@iniciativa.turno_comision = tds[i+1].text.strip.squeeze(' ')
-		@iniciativa.sinopsis = tds[i+2].text.strip.squeeze(' ')
-		@iniciativa.tramite = tds[i+3].text.strip.squeeze(' ')
-		@iniciativas << @iniciativa
-	    end
-	    i += 4
-	end while i < tds.length
-    end
+		nbsp = Nokogiri::HTML('&nbsp;').text
 
-    rabl :iniciativas
-
-end
-
-
-get '/api/diputado/:id/proposiciones' do
-
-    @proposiciones = Array.new
-
-    for t in 1..6 do
-		doc = Nokogiri::HTML(open("http://sitl.diputados.gob.mx/LXII_leg/proposiciones_por_pernplxii.php?iddipt=#{params[:id]}&pert=#{t}"))
-		tds = doc.css("table")[1].css("td")
-		
-		i = 0
-		begin
-		    if !tds[i].text.strip.match(/^PROPOSICI/)
-				@proposicion = Proposicion.new
-				@proposicion.nombre = tds[i].text.strip.squeeze(' ')
-				@proposicion.turno_comision = tds[i+1].text.strip.squeeze(' ')
-				@proposicion.resolutivos_proponente = tds[i+2].text.strip.squeeze(' ')
-				@proposicion.resolutivos_aprobados = tds[i+3].text.strip.squeeze(' ')
-				@proposicion.tramite = tds[i+4].text.strip.squeeze(' ')
-				@proposiciones << @proposicion
-		    end
-		    i += 5
-		end while i < tds.length
-    end
-
-    rabl :proposiciones
-end
-
-
-get '/api/diputado/:id/votaciones' do
-    fecha = nil
-    @votaciones = Array.new
-
-    for t in 1..6 do
-		doc = Nokogiri::HTML(open("http://sitl.diputados.gob.mx/LXII_leg/votaciones_por_pernplxii.php?iddipt=#{params[:id]}&pert=#{t}"))
-		trs = doc.css("table table")[1].css("tr")
-		
-		trs.each do |tr|
-		    if tr.at_css("td.TitulosVerde")
-				fecha = tr.at_css("td.TitulosVerde").text
-		    end
-
-		    if tr.css("td").length > 1
-				@votacion = Votacion.new
-				@votacion.fecha = fecha
-				@votacion.titulo = tr.css("td")[1].text.strip.squeeze(' ')
-				@votacion.voto = tr.css("td")[3].text.strip.squeeze(' ')
-				@votaciones << @votacion
-		    end
-		end
-    end
-
-    rabl :votaciones
-end
-
-
-get '/api/diputado/:id/asistencias' do
-    fecha = nil
-    
-    @asistencias = Array.new
-
-    for t in 1..6 do
-		doc = Nokogiri::HTML(open("http://sitl.diputados.gob.mx/LXII_leg/asistencias_por_pernplxii.php?iddipt=#{params[:id]}&pert=#{t}"))
-		trs = doc.css("table table")[1].css("tr table")[2]
-		
-		trs.css("table table").each do |mes|
-		    
-			mes.css("td").each do |td|
-				if td.at_css("span.TitulosVerde")
-				    fecha = td.at_css("span.TitulosVerde").text
-				end
-				
-				if td.attr("bgcolor") == '#D6E2E2'
-				    @asistencia = Asistencia.new
-				    @asistencia.fecha = fecha.strip.squeeze(' ')
-				    @asistencia.dia = td.at_css("font").inner_html.split("<br>")[0].strip
-				    @asistencia.status = td.at_css("font").inner_html.split("<br>")[1].strip
-				    @asistencias << @asistencia
-				end
-		    end
-		end
-    end
-
-    @asistenciasHash = Hash.new{|h, k| h[k] = []}
-    @asistencias.each do |a|
-    	@asistenciasHash[a.fecha] << a
-    end
-
-    require 'json'
-    @asistenciasHash.to_json
-    #rabl :asistencias
-end
-
-
-get '/api/comision/:id/integrantes' do
-	cargo = nil
-	@comision_integrantes = Array.new
-	doc = Nokogiri::HTML(open("http://sitl.diputados.gob.mx/LXII_leg/integrantes_de_comisionlxii.php?comt=#{params[:id]}"))
-
-	doc.css("table table")[1].css("tr").each do |tr|
-
-		if tr.at_css("td.TitulosVerde")
-		    cargo = tr.at_css("td.TitulosVerde").text
-		end
-
-		if tr.css("td").length > 3
-
-			a = tr.css("td").first.at_css("a")
-
-			if a and a.attr("href").match(/^curricula\.php\?dipt=\d+/)
-				@comision_integrante = ComisionIntegrante.new
-				@comision_integrante.id = a.attr("href").gsub('curricula.php?dipt=', '')
-				@comision_integrante.nombre = a.text.strip.squeeze(' ')
-				@comision_integrante.partido = tr.css("td")[1].text.strip.squeeze(' ')
-				@comision_integrante.entidad = tr.css("td")[2].text.strip.squeeze(' ')
-				@comision_integrante.ubicacion = tr.css("td")[3].text.strip.squeeze(' ')
-				@comision_integrante.extension = tr.css("td")[4].text.strip.squeeze(' ')
-				@comision_integrante.cargo = cargo
-			 	@comision_integrantes << @comision_integrante
+		if tr.at_css('td').text.strip == 'Totales'
+			if calculaRendimiento
+				combustibleTotal += tr.next.next.css('td')[4].text.gsub(nbsp, ' ').strip.to_f
 			end
-
+			costoTotal = peajeTotal + combustibleTotal
+			break
 		end
+
+		nombre = tr.css('td')[0].text.gsub(nbsp, ' ').strip
+		estado = tr.css('td')[1].text.gsub(nbsp, ' ').strip
+		carretera = tr.css('td')[2].text.gsub(nbsp, ' ').strip
+
+		if !zonasUrbanas
+			next unless carretera != 'Zona Urbana'
+		end
+
+		longitud = tr.css('td')[3].text.gsub(nbsp, ' ').strip
+		distanciaTotal += longitud.to_f
+
+		tiempo = tr.css('td')[4].text.gsub(nbsp, ' ').strip
+		horas, minutos = tiempo.split(':')
+		tiempoTotal += horas.to_i * 60 + minutos.to_i
+
+		caseta = tr.css('td')[5].text.gsub(nbsp, ' ').strip
+		costo = tr.css('td')[6].text.gsub(nbsp, ' ').strip
+		peajeTotal += costo.to_f
+
+		tramo['nombre'] = nombre
+		tramo['estado'] = estado
+		tramo['carretera'] = carretera
+		tramo['longitud'] = longitud
+		tramo['tiempo'] = tiempo
+		tramo['caseta'] = caseta
+		tramo['costo'] = costo
+	  	tramos.push(tramo)
 
 	end
 
-	rabl :comision_integrantes
+	response = {
+	  tramos: tramos,
+	  puntos: puntos,
+	  distancia: distanciaTotal,
+	  tiempo: tiempoTotal,
+	  peaje: peajeTotal,
+	  combustible: combustibleTotal,
+	  costo: costoTotal
+	}
+
+	response.to_json
+
 end
